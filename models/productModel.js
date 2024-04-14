4fbcd588e14dfaece4beb6c40b24a7e18d8ea11c
nithinraj1999 <vnithinraj99@gmail.com>
2024-04-14 11:52:52 +0530

    change Models to model

---
+++ models/productModel.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose")
+
+
+const productSchema = mongoose.Schema({
+    
+    name:{
+        type:String,
+        required:true,
+    },
+    subcategory_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Category",
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    manufacturer:{
+        type:String,
+        required:true
+    },
+    quantity:{
+        type:Number,
+        required:true
+    },
+    list:{
+        type:Boolean,
+        default:true
+    },
+    offer:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"offer",
+        
+    },
+    product_image:[
+        {
+                filename:{
+                    type:String,
+                    required:true
+                },
+                    path: {
+                    type:String,
+                    required:true
+                },
+                    resizedFile: {
+                    type:String,
+                    required:true,
+                   
+                    }
+        }
+    ]
+})
+
+ 
+module.exports = mongoose.model("products",productSchema)
